1f84fd3b0799ee001e0ba31869bec1a5e3115876
Samuel Moreira Ferreira <samuel.mferreira205@gmail.com>
2025-06-14 08:53:35 -0300

    MODIFY Funcionamento das funções para executarem menos ações no Banco de Dados

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 class Product:
 
     @staticmethod
-    def get_by_id (id, fulldata:bool=False):
+    def get_by_id (id):
 
         connection_db = Connection.create()
         cursor = connection_db.cursor(dictionary=True)
@@ -18,81 +18,113 @@ class Product:
             cursor.execute(
                 
                 """
-                SELECT tb_products.name, tb_products.description, tb_products.price, tb_products.quantity, tb_products.rating, tb_products.type FROM tb_products 
+                SELECT,
+
+                tb_products.product_id AS "id",
+
+                tb_products.name.
+                tb_products.description.
+
+                tb_products.price,
+                tb_products.quantity,
+                tb_products.rating,
+
+                tb_products_types.type AS "product_type",
+                tb_monkeys_classes.class AS "monkey_class",
+                GROUP_CONCAT(tb_bloons_types.type) AS "bloon_types",
+
+                GROUP_CONCAT(tb_products_images.image_url) AS "images"
+
+                FROM tb_products
+
+                JOIN tb_products_types 
+                    ON tb_products.type = tb_products_types.type_id
+
+                LEFT JOIN tb_monkeys 
+                    ON tb_products.product_id = tb_monkeys.product_id
+                LEFT JOIN tb_monkeys_classes 
+                    ON tb_monkeys.class = tb_monkeys_classes.class_id
+
+                LEFT JOIN tb_bloons 
+                    ON tb_products.product_id = tb_bloons.product_id
+                LEFT JOIN tb_bloon_type_relation 
+                    ON tb_bloons.bloon_id = tb_bloon_type_relation.bloon_id
+                LEFT JOIN tb_bloons_types 
+                    ON tb_bloon_type_relation.type_id = tb_bloons_types.type_id
+
+                LEFT JOIN tb_products_images 
+                    ON tb_products.product_id = tb_products_images.product_id
+
                 WHERE tb_products.product_id = %s;
-                """,
+                """
 
                 (id, )
                 
             )
 
-            product_data = cursor.fetchone()
+            return cursor.fetchone()
 
-            if product_data:
-
-                data |= product_data
-            
-            #region Full Data
+            # #region Full Data
     
-            if fulldata:
+            # if fulldata:
 
-                # Macacos
+            #     # Macacos
 
-                if data['type'] == 1:
+            #     if data['type'] == 1:
 
-                    cursor.execute(
+            #         cursor.execute(
 
-                        """
-                        SELECT tb_monkeys_classes.class, tb_monkeys_classes.description AS 'class-description' FROM tb_monkeys_classes
-                        INNER JOIN tb_monkeys 
-                            ON tb_monkeys.monkey_id = tb_monkeys_classes.class_id
-                        WHERE tb_monkeys.product_id = %s;
-                        """,
+            #             """
+            #             SELECT tb_monkeys_classes.class, tb_monkeys_classes.description AS 'class-description' FROM tb_monkeys_classes
+            #             INNER JOIN tb_monkeys 
+            #                 ON tb_monkeys.monkey_id = tb_monkeys_classes.class_id
+            #             WHERE tb_monkeys.product_id = %s;
+            #             """,
 
-                        (id, )
+            #             (id, )
 
-                    )
+            #         )
 
-                    monkey_data = cursor.fetchone()
+            #         monkey_data = cursor.fetchone()
 
-                    if monkey_data:
+            #         if monkey_data:
 
-                        data |= monkey_data
+            #             data |= monkey_data
 
-                # Bloons
+            #     # Bloons
 
-                else:
+            #     else:
 
-                    cursor.execute(
+            #         cursor.execute(
 
-                        """
-                        SELECT tb_bloons_types.type FROM tb_bloons_types
-                        INNER JOIN tb_bloon_type_relation
-                            ON tb_bloon_type_relation.type_id = tb_bloons_types.type_id
-                        WHERE tb_bloons.product_id = %s;
-                        """,
+            #             """
+            #             SELECT tb_bloons_types.type FROM tb_bloons_types
+            #             INNER JOIN tb_bloon_type_relation
+            #                 ON tb_bloon_type_relation.type_id = tb_bloons_types.type_id
+            #             WHERE tb_bloons.product_id = %s;
+            #             """,
 
-                        (id, )
+            #             (id, )
 
-                    )
+            #         )
 
-                    bloon_data = cursor.fetchone()
+            #         bloon_data = cursor.fetchone()
 
-                    if bloon_data:
+            #         if bloon_data:
 
-                        data |= bloon_data
+            #             data |= bloon_data
 
-            #endregion
+            # #endregion
 
-            # Imagens
+            # # Imagens
 
-            images = Product.get_images(id)
+            # images = Product.get_images(id)
 
-            if images:
+            # if images:
 
-                data['images'] = images
+            #     data['images'] = images
 
-            return data
+            # return data
 
         except Error as e:
 
@@ -113,12 +145,16 @@ class Product:
 
         try:
 
-            data = []
-
             cursor.execute(
 
                 """
-                SELECT tb_products_images.image_url FROM tb_products_images
+                SELECT 
+
+                tb_products.product_id AS "id",
+                GROUP_CONCAT(tb_product s_images.image_url) AS "images"
+
+                FROM tb_products_images
+
                 WHERE tb_products_images.image_id = %s;
                 """,
 
@@ -126,11 +162,7 @@ class Product:
 
             )
 
-            for product_image in cursor.fetchall():
-
-                data.append(product_image['image_url'])
-
-            return data
+            return cursor.fetchone()
         
         except Error as e:
 
