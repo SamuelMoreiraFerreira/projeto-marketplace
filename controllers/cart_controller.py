ad7ccb6115b4383bee3aac4527b585f621164f63
Samuel Moreira Ferreira <samuel.mferreira205@gmail.com>
2025-06-16 09:57:54 -0300

    ADD 'cart_controller' -> Funções create, add-item, delete-product

--- controllers/cart_controller.py
+++ controllers/cart_controller.py
@@ -3,6 +3,56 @@ from mysql.connector import Error
 
 class Carts:
 
+    @staticmethod
+    def create(**cart_data):
+
+        # cart_data:
+        #
+        # - user_email
+        # - products: [{ 'product_id': '', 'quantity': '' }, ...]
+
+        connection_db = Connection.create()
+        cursor = connection_db.cursor()
+
+        try:
+
+            cursor.execute('INSERT INTO tb_shopping_cart (user_email) VALUES (%s);', (cart_data.get('user'), ))
+
+            cart_id = cursor.lastrowid
+
+            for product in cart_data.get('products'):
+
+                cursor.execute(
+                    
+                    """
+                    INSERT INTO tb_cart_products (cart_id, product_id, quantity)
+                    VALUES (%s, %s, %s);
+                    """, 
+                    
+                    (
+                        cart_id, 
+                        product.id,
+                        product.quantity
+                    )
+                    
+                )   
+
+            connection_db.commit()
+
+            return True
+
+        except Error as e:
+
+            print(f'Erro - Cart "create": {e}')
+            
+            return False
+
+        finally:
+
+            cursor.close()
+            connection_db.close()
+
+    @staticmethod
     def get_by_id (id):
 
         connection_db = Connection.create()
@@ -17,14 +67,17 @@ class Carts:
                     tb_shopping_cart.user_email,
                     GROUP_CONCAT(
                         CONCAT(
-                            tb_cart_products.cart_product_id, ":",
-                            tb_cart_products.product_id, ":",
+                            tb_cart_products.cart_product_id, ':',
+                            tb_cart_products.product_id, ':',
+                            tb_products.name, ':',
                             tb_cart_products.quantity                       
-                        ) SEPARATOR "|"
-                    ) AS "products"
+                        ) SEPARATOR '|'
+                    ) AS 'products'
                 FROM tb_shopping_cart
                 INNER JOIN tb_cart_products
                     ON tb_shopping_cart.cart_id = tb_cart_products.cart_id
+                INNER JOIN tb_products
+                    ON tb_cart_products.product_id = tb_products.product_id
                 WHERE tb_shopping_cart.cart_id = %s;
                 """,
 
@@ -34,30 +87,37 @@ class Carts:
 
             response = cursor.fetchone()
 
-            data = {
+            if response['user_email'] is not None:
 
-                'user': response['user_email'],
+                data = {
 
-                'products': []
+                    'cart_id': id,
+                    'user': response['user_email'],
 
-            }
+                    'products': []
 
-            keys = ('cart_product_id', 'product_id', 'quantity')
+                }
 
-            for product in (response['products'].split('|')):
+                keys = ('cart_product_id', 'product_id', 'product', 'quantity')
 
-                data['products'].append(
+                for product in (response['products'].split('|')):
 
-                    dict.fromkeys(
-                        keys,
-                        tuple(product.split(':'))
+                    data['products'].append(
+
+                        # 'zip()' -> Agrupa itens em pares correspondendo por posição
+
+                        dict(zip(
+                            keys,
+                            tuple(product.split(':'))
+                        ))
+
                     )
 
-                )
+                return data
 
-            print(data)
-
-            return data
+            else:
+            
+                return False
         
         except Error as e:
 
@@ -70,18 +130,49 @@ class Carts:
             cursor.close()
             connection_db.close()
 
+    def add_item(cart_id, product_id, quantity):
+
+        connection_db = Connection.create()
+        cursor = connection_db.cursor()
+
+        try:
+
+            cursor.execute('INSERT INTO tb_cart_products (cart_id, product_id, quantity) VALUES (%s, %s, %s)', (cart_id, product_id, quantity))
+
+            connection_db.commit()
+
+            return True
+        
+        except Error as e:
+
+            print(f'Erro - Cart "add_item": {e}')
+
+            return False
+        
+        finally:
+
+            cursor.close()
+            connection_db.close()
+
+    @staticmethod
     def delete (id):
 
         connection_db = Connection.create()
-        cursor = connection_db.cursor(dictionary=True)
+        cursor = connection_db.cursor()
 
         try:
 
             cursor.execute('DELETE FROM tb_cart_products WHERE tb_cart_products.cart_id = %s', (id, ))
 
-            cursor.commit()
+            connection_db.commit()
 
-            return True
+            if cursor.rowcount > 0:
+
+                return True
+            
+            else:
+
+                return False
         
         except Error as e:
 
@@ -94,18 +185,25 @@ class Carts:
             cursor.close()
             connection_db.close()
 
-    def delete_product (cart_id, product_id):
+    @staticmethod
+    def delete_product (cart_id, cart_product_id):
 
         connection_db = Connection.create()
-        cursor = connection_db.cursor(dictionary=True)
+        cursor = connection_db.cursor()
 
         try:
 
-            cursor.execute('DELETE FROM tb_cart_products WHERE tb_cart_products.product_id = %s AND tb_cart_products.cart_id = %s', (product_id, cart_id))
+            cursor.execute('DELETE FROM tb_cart_products WHERE tb_cart_products.cart_product_id = %s AND tb_cart_products.cart_id = %s', (cart_product_id, cart_id))
 
-            cursor.commit()
+            connection_db.commit()
 
-            return True
+            if cursor.rowcount > 0:
+
+                return True
+            
+            else:
+
+                return False
         
         except Error as e:
 
